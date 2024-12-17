1ff73c9a3602c0937799670996ef57171a72337c
Source Coty <coty.massmann@gmail.com>
2024-12-17 21:32:23 +0700

    Change brand colors

--- src/assets/js/pricing.js
+++ src/assets/js/pricing.js
@@ -1,11 +1,16 @@
-const toggle = document.querySelector("#pricing-356 .cs-toggle");
+const toggle = document.querySelector("#pricing-387 .cs-toggle");
+const monthlyItems = Array.from(document.querySelectorAll(".cs-monthly-only"));
         const option1 = document.querySelector("#pricing .cs-toggle-group #cs-option1");
         const option2 = document.querySelector("#pricing .cs-toggle-group #cs-option2");
-        const optionGroup = Array.from(document.querySelectorAll('.cs-option-group'))
+        const optionGroup = Array.from(document.querySelectorAll('.cs-option-group'));
+
 
         toggle.addEventListener('click', (e) => { 
             for (const item of optionGroup) {
                 item.classList.toggle("option2-active");
+            }
+            for (const item of monthlyItems) {
+                item.classList.toggle("cs-disabled");
             }
             toggle.classList.toggle("active");
         });
@@ -13,12 +18,20 @@ const toggle = document.querySelector("#pricing-356 .cs-toggle");
             for (const item of optionGroup) {
                 item.classList.remove("option2-active");
             }
+            for (const item of monthlyItems) {
+                item.classList.remove("cs-disabled");
+            }
             toggle.classList.remove("active");
+            monthlyItems.classList.remove("cs-disabled")
         });
         option2.addEventListener('click', (e) => { 
             for (const item of optionGroup) {
                 item.classList.add("option2-active");
             }
+            for (const item of monthlyItems) {
+                item.classList.toggle("cs-disabled");
+            }
             toggle.classList.add("active");
+            monthlyItems.classList.add("cs-disabled")
         });
-                                
+                                            
