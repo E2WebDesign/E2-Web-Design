c9402214ef0a86eac9ba338f1f8a20064bd6ce92
Source Coty <coty.massmann@gmail.com>
2024-12-26 17:44:11 +0700

    Update gallery images

--- src/assets/js/pricing.js
+++ src/assets/js/pricing.js
@@ -1,7 +1,7 @@
 const toggle = document.querySelector("#pricing-387 .cs-toggle");
 const monthlyItems = Array.from(document.querySelectorAll(".cs-monthly-only"));
-        const option1 = document.querySelector("#pricing .cs-toggle-group #cs-option1");
-        const option2 = document.querySelector("#pricing .cs-toggle-group #cs-option2");
+        const option1 = document.querySelector("#pricing .cs-toggle-group #cs-option1-387");
+        const option2 = document.querySelector("#pricing .cs-toggle-group #cs-option2-387");
         const optionGroup = Array.from(document.querySelectorAll('.cs-option-group'));
 
 
@@ -22,7 +22,7 @@ const monthlyItems = Array.from(document.querySelectorAll(".cs-monthly-only"));
                 item.classList.remove("cs-disabled");
             }
             toggle.classList.remove("active");
-            monthlyItems.classList.remove("cs-disabled")
+            monthlyItems.classList.remove("cs-disabled");
         });
         option2.addEventListener('click', (e) => { 
             for (const item of optionGroup) {
@@ -32,6 +32,5 @@ const monthlyItems = Array.from(document.querySelectorAll(".cs-monthly-only"));
                 item.classList.toggle("cs-disabled");
             }
             toggle.classList.add("active");
-            monthlyItems.classList.add("cs-disabled")
+            monthlyItems.classList.add("cs-disabled");
         });
-                                            
